076a1de10dc67845d64d99345a5d85cce6bcaefc
Ubuntu <azureuser@Makefiledemo.dzhp2rt5az0uxkilx1tidkflke.ax.internal.cloudapp.net>
2021-06-14 13:40:49 +0000

    updated submissions

---
+++ submission.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import pandas as pd
+import sqlite3
+import  time
+
+start_time = time.time()
+
+# import data and convert 'dt_start' &  'dt_end' to datetime format
+tarrif = pd.read_csv('./data/input/tariffs.csv')
+tarrif['dt_start'] = pd.to_datetime(tarrif['dt_start'])
+tarrif['dt_end'] = pd.to_datetime(tarrif['dt_end'])
+
+session = pd.read_csv('./data/input/sessions.csv')
+session['dt_start'] = pd.to_datetime(session['dt_start'])
+session['dt_end'] = pd.to_datetime(session['dt_end'])
+
+
+# TODO :  Make a function to do this.
+# connect a sqlite library to perform joins
+conn = sqlite3.connect("./data/output/library.db")
+session.to_sql('session',conn, index=False)
+tarrif.to_sql('tarrif',conn,index=False)
+
+qry = '''
+select s.id, s.dt_start, s.dt_end , s.energy, t.energy_fee, t.parking_fee
+from session s join tarrif t
+WHERE (s.dt_start between t.dt_start and t.dt_end)
+and (s.dt_end between t.dt_start and t.dt_end)'''
+
+df = pd.read_sql_query(qry, conn)
+
+df['dt_start'] = pd.to_datetime(df['dt_start'])
+df['dt_end'] = pd.to_datetime(df['dt_end'])
+
+# TODO: make a function for this.
+# finally manipulate data to get the desired output
+df['minutes'] = (df['dt_end'] - df['dt_start']).dt.components.minutes
+
+# df['minutes'] = df['minutes'].dt.components.minutes
+
+df['tariff_cost'] =  (((df.energy * df.energy_fee) + (df.minutes*df.parking_fee)) * 1.15)
+
+round(df.groupby('id')['tariff_cost'].sum(),2).to_csv('./data/output/tariff_cost.csv')
+
+print('tarrif_cost file is sucessfully generated')
+print('time taken for overall operations is {} mins'.format(round(time.time()-start_time),2)/60))
