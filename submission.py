bfe981ecb94e302d58a906ad3d7422da5cc7b1c4
javedhassans <javedhassans@outlook.com>
2021-06-15 13:03:03 +0200

    removed duplicatecolumns

--- submission.py
+++ submission.py
@@ -2,7 +2,7 @@ import sys
 import os
 import pandas as pd
 import sqlite3
-import  time
+import time
 
 start_time = time.time()
 
@@ -14,34 +14,26 @@ tarrif['dt_end'] = pd.to_datetime(tarrif['dt_end'])
 session = pd.read_csv('./data/input/sessions.csv')
 session['dt_start'] = pd.to_datetime(session['dt_start'])
 session['dt_end'] = pd.to_datetime(session['dt_end'])
-
+session['minutes'] = (session['dt_end'] - session['dt_start']).dt.components.minutes
 
 # TODO :  Make a function to do this.
 # connect a sqlite library to perform joins
 conn = sqlite3.connect("./data/output/library.db")
-session.to_sql('session',conn, index=False)
-tarrif.to_sql('tarrif',conn,index=False)
+session.to_sql('session', conn, index=False, if_exists='replace' )
+tarrif.to_sql('tarrif', conn, index=False, if_exists='replace')
 
 qry = '''
-select s.id, s.dt_start, s.dt_end , s.energy, t.energy_fee, t.parking_fee
+select s.id, s.energy, s.minutes,t.energy_fee, t.parking_fee
 from session s join tarrif t
 WHERE (s.dt_start between t.dt_start and t.dt_end)
 and (s.dt_end between t.dt_start and t.dt_end)'''
 
 df = pd.read_sql_query(qry, conn)
 
-df['dt_start'] = pd.to_datetime(df['dt_start'])
-df['dt_end'] = pd.to_datetime(df['dt_end'])
 
-# TODO: make a function for this.
-# finally manipulate data to get the desired output
-df['minutes'] = (df['dt_end'] - df['dt_start']).dt.components.minutes
+df['tariff_cost'] = (((df.energy * df.energy_fee) + (df.minutes * df.parking_fee)) * 1.15)
 
-# df['minutes'] = df['minutes'].dt.components.minutes
-
-df['tariff_cost'] =  (((df.energy * df.energy_fee) + (df.minutes*df.parking_fee)) * 1.15)
-
-round(df.groupby('id')['tariff_cost'].sum(),2).to_csv('./data/output/tariff_cost.csv')
+round(df.groupby('id')['tariff_cost'].sum(), 2).to_csv('./data/output/tariff_cost.csv')
 
 print('tarrif_cost file is sucessfully generated')
-print('time taken for overall operations is {} mins'.format(round(time.time()-start_time),2)/60)
+print('time taken for overall operations is {} mins'.format(round(time.time() - start_time), 2) / 60)
